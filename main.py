d9001b5b619107af412fbd876b07642a299a7fd4
mxngjxa <mingjia.guan@gmail.com>
2024-11-22 10:04:21 -0500

    basic structure

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import requests
+from bs4 import BeautifulSoup as bs
+from pprint import pp
+
 URL = 'https://www.cdn.geeksforgeeks.org/data-structures/'
-r = requests.get(URL)
-print(r.content)
+alaska = 'https://education.alaska.gov/DOE_Rolodex/SchoolCalendar/DistrictAndSchoolInfo/DistrictDetails'
+r = requests.get(alaska)
+
+soup = bs(r.content, 'html5lib')
+
+print(soup.prettify())
